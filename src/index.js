57c063a62377b232146f83f78e0e87a88c27061c
Cemsina Güzel <cemsina1997@hotmail.com>
2018-09-23 23:33:24 +0300

    first

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
+/**
+ * pdfstudio - PDF 
+ * Copyright (c) 2018 Cemsina Güzel <cemsina@cemsina.com>, https://github.com/cemsina
+ */
 import $ from "jquery";
 var pdfstudio = require("./util/pdfstudio");
 new pdfstudio();
